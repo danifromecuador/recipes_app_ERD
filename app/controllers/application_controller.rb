299177df8097516bd7e7024a09a539fad6fabb30
dani <danifromecuador@gmail.com>
2023-11-28 23:10:56 -0500

    Allow requests from any origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :set_cors_headers
+
+  private
+
+  def set_cors_headers
+    # Allow requests from any origin
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+    # Additional headers for allowing specific HTTP methods
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+
+    # Additional headers for allowing specific HTTP headers
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+
+    # Optional: Allow credentials (cookies, authorization headers, etc.)
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+  end
 end
