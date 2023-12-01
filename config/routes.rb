dcbe5dfa65a9bc5d645b312a55dce67e3f9f07c6
dani <danifromecuador@gmail.com>
2023-12-01 17:11:53 -0500

    Add routes or endpoints for foods and recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'health', to: 'health#health'
   resources :users, only: [:index, :show, :create, :destroy]
+  resources :foods, only: [:index, :create, :destroy]
   resources :recipes, only: [:index, :show, :create, :edit, :destroy]
+  resources :recipe_foods, only: [:index, :show, :create, :edit, :destroy]
 end
