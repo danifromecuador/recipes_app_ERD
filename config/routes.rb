408cd37fb9c6e5fe55ba0b0e59ddaee36f3476ff
dani <danifromecuador@gmail.com>
2023-12-01 16:35:20 -0500

    Create index endpoint for /recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'health', to: 'health#health'
   resources :users, only: [:index, :show, :create, :destroy]
+  resources :recipes, only: [:index, :show, :create, :edit, :destroy]
 end
